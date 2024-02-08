2ec282d28ae5dfde1a225a395edc17d020218c6b
vguenoff <vguenoff@gmail.com>
2024-02-08 08:32:05 +0200

    feat: srp

--- SOLID/1. Single Responsibility (SRP)/after.ts
+++ SOLID/1. Single Responsibility (SRP)/after.ts
@@ -1,3 +1,10 @@
+// What's SOLID meaning?
+// In software engineering, SOLID is a mnemonic acronym for five design principles intended to make software designs more understandable, flexible, and maintainable. The principles are a subset of many principles promoted by American software engineer and instructor Robert C. Martin, first introduced in his 2000 paper Design Principles and Design Patterns.
+
+// 1. Single Responsibility (SRP)
+// ! There should never be more than one reason for a class to change.
+// ! Every class should have only one responsibility.
+
 class Note {
   private static count = 0
   public readonly id: number
